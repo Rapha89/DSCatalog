840800ff03ed3a73c2f9b93582ec562070993f37
Raphael Vacari <raphaelvacari7@gmail.com>
2024-02-20 20:37:08 -0300

    H2 e camadas

---
+++ src/main/java/com/dscatalog/controllers/CategoryController.java
@@ -0,0 +1,25 @@
+package com.dscatalog.controllers;
+
+import com.dscatalog.entities.Category;
+import com.dscatalog.services.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/categories")
+public class CategoryController {
+    @Autowired
+    private CategoryService service;
+    @GetMapping
+    public ResponseEntity<List<Category>> findAll(){
+        List<Category> list = service.findAll();
+        return ResponseEntity.ok().body(list);
+    }
+
+}
