1bfde6816c6b416328e2bff33863b22c2ff669b5
Raphael Vacari <raphaelvacari7@gmail.com>
2024-02-21 15:46:43 -0300

    Exception

--- src/main/java/com/dscatalog/services/CategoryService.java
+++ src/main/java/com/dscatalog/services/CategoryService.java
@@ -3,6 +3,7 @@ package com.dscatalog.services;
 import com.dscatalog.dto.CategoryDTO;
 import com.dscatalog.entities.Category;
 import com.dscatalog.repositories.CategoryRepository;
+import com.dscatalog.services.exceptions.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -24,7 +25,7 @@ public class CategoryService {
     @Transactional(readOnly = true)
     public CategoryDTO findById(Long id) {
         Optional<Category> obj = repository.findById(id);
-        Category entity = obj.get();
+        Category entity = obj.orElseThrow(() -> new EntityNotFoundException("Entity Not Found"));
         return new CategoryDTO(entity);
     }
 }
